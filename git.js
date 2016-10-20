b7aa5ba368744da326e82e3b7cf398c8dc9496c1
Hooky <hooky@powershop.co.nz>
2016-10-20 14:07:52 +1300

    begin real git commands

--- git.js
+++ git.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const cp = require('child_process');
+let gitPath = 'git';
+let gitRepoBase = '';
+const gitRun = cmdArgs => {
+  return new Promise( (resolve, reject) => {
+    const subProc = cp.spawn(gitPath, cmdArgs, {
+      
+    })
+  })
+}
+
+exports.setGitPath = newPath => gitPath = newPath;
+exports.setGitRepoBase = newBase => gitRepoBase = newBase;
+
 exports.branchList = () => {
+  
+  
   return Promise.resolve([{
     msg: "hi J",
     sha: "abcdef",
