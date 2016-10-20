b1b95eed6f1b3811279f6731e1726a2215177288
Hooky <hooky@powershop.co.nz>
2016-10-20 15:33:58 +1300

    Continuing git stuff

--- git.js
+++ git.js
@@ -1,26 +1,39 @@
 'use strict';
 
+/* exports:
+
+setGitPath
+setGitRepoBase
+branchList
+*/
+
 const cp = require('child_process');
 let gitPath = 'git';
-let gitRepoBase = '';
+let gitRepoBase = null;
 const gitRun = cmdArgs => {
 	return new Promise((resolve, reject) => {
+		
+		console.log("calling git", cmdArgs, gitRepoBase || process.cwd(), process.pwd)
 		const subProc = cp.spawn(gitPath, cmdArgs, {
-
-		})
-	})
+			cwd: gitRepoBase || process.cwd(),
+			env: process.env
+		});
+		let data = [];
+		subProc.stdout.on('data', 
+		d => 
+		{
+			console.log("data", d.toString());
+			data.push(d)
+		});
+		subProc.stderr.on('data', d =>console.log("error:", d.toString()));
+		subProc.on('error', e => console.log(e))
+		subProc.on('exit', () => resolve(Buffer.concat(data)
+			.toString()));
+	});
 }
 
 exports.setGitPath = newPath => gitPath = newPath;
 exports.setGitRepoBase = newBase => gitRepoBase = newBase;
-
-exports.branchList = () => {
-	return Promise.resolve([{
-		name: "some_branch",
-		msg: "a very interesting commit",
-		sha: "a1b2c3d"
-	}]);
-};
 
 exports.currentBranch = () => {
 	return Promise.resolve({
@@ -28,4 +41,22 @@ exports.currentBranch = () => {
 		msg: "master branch",
 		sha: "pooppooooooopoo"
 	});
+}
+
+exports.branchList = () => {
+	return gitRun(['branch', '-v', '--sort=-committerdate'])
+		.then(resultString => {
+			const matcher = /[\s\*]*(.+?)\s*([a-fA-F0-9]*)\s*(.*)/y;
+			let match = matcher.exec(resultString);
+			let result = [];
+			while (match) {
+				result.push({
+					name: match[1],
+					sha: match[2],
+					msg: match[3]
+				});
+				match = matcher.exec(resultString);
+			}
+			return result;
+		})
 };
