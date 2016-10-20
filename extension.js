1c7f099e7efb9498a13c19f10315e94fe5352cc5
Jordane Lew <jordane.lew@powershop.co.nz>
2016-10-20 14:52:13 +1300

    Some cleanup to prefer consts and lets

--- extension.js
+++ extension.js
@@ -2,10 +2,10 @@
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
-var git = require('./git');
+const vscode = require('vscode');
+const git = require('./git');
 
-var QuickPickDiffItem = function(diffItem) {
+const QuickPickDiffItem = function(diffItem) {
 	this.name = diffItem.name;
 	this.sha = diffItem.sha;
 	this.msg = diffItem.msg;
@@ -16,13 +16,13 @@ var QuickPickDiffItem = function(diffItem) {
 
 const chooseBranch = () => git.branchList()
 	.then(branchList => {
-			var defaultBranch = branchList.length ? branchList[0].name : 'master';
+			let defaultBranch = branchList.length ? branchList[0].name : 'master';
 
 			return vscode.window.showQuickPick(
-					branchList.map(item => new QuickPickDiffItem(item)), {
-						placeHolder: defaultBranch
-					}
-				);
+				branchList.map(item => new QuickPickDiffItem(item)), {
+					placeHolder: defaultBranch
+				}
+			);
 		},
 		() => null);
 
