5bf7856beff8bab640ff1aef3bde31302d601437
Jordane Lew <jordane.lew@powershop.co.nz>
2016-10-20 14:06:41 +1300

    Made dummy branches less shit

--- git.js
+++ git.js
@@ -1,11 +1,9 @@
 'use strict';
 
 exports.branchList = () => {
-  return Promise.resolve([{
-    msg: "hi J",
-    sha: "abcdef",
-    name: "b_1"
-  }]);
-}
-
-
+	return Promise.resolve([{
+		name: "some_branch",
+		msg: "a very interesting commit",
+		sha: "a1b2c3d"
+	}]);
+};
