eedec8608420d134a8f2a32ade7a1f594409d43c
Hooky <hooky@powershop.co.nz>
2016-10-20 16:14:48 +1300

    Better gitting

--- git.js
+++ git.js
@@ -2,9 +2,10 @@
 
 /* exports:
 
-setGitPath
-setGitRepoBase
-branchList
+setGitPath("path/to/git")
+setGitRepoBase("basedir")
+branchList()
+diffState("reviewBranch", "targetBranch")
 */
 
 const cp = require('child_process');
@@ -12,41 +13,69 @@ let gitPath = 'git';
 let gitRepoBase = null;
 const gitRun = cmdArgs => {
 	return new Promise((resolve, reject) => {
-		
-		console.log("calling git", cmdArgs, gitRepoBase || process.cwd(), process.pwd)
+
 		const subProc = cp.spawn(gitPath, cmdArgs, {
 			cwd: gitRepoBase || process.cwd(),
 			env: process.env
 		});
 		let data = [];
-		subProc.stdout.on('data', 
-		d => 
-		{
-			console.log("data", d.toString());
-			data.push(d)
+		let err = [];
+		subProc.stdout.on('data', d => data.push(d));
+		subProc.stderr.on('data', d => err.push(d));
+		subProc.on('exit', () => {
+			if (!data.length) {
+				if (err.length) reject(Buffer.concat(err)
+					.toString());
+			} else resolve(Buffer.concat(data)
+				.toString());
 		});
-		subProc.stderr.on('data', d =>console.log("error:", d.toString()));
-		subProc.on('error', e => console.log(e))
-		subProc.on('exit', () => resolve(Buffer.concat(data)
-			.toString()));
 	});
-}
+};
 
 exports.setGitPath = newPath => gitPath = newPath;
 exports.setGitRepoBase = newBase => gitRepoBase = newBase;
 
 exports.currentBranch = () => {
-	return Promise.resolve({
-		name: "master",
-		msg: "master branch",
-		sha: "pooppooooooopoo"
-	});
-}
+	return gitRun(['branch'])
+		.then(resultString => {
+			const matcher = /\s*\*\s+(.+?)/y;
+			let match = matcher.exec(resultString);
+			if (!match) throw "No current branch found";
+			return {
+				name: match[1],
+				sha: match[2],
+				msg: match[3]
+			};
+		});
+};
+
+exports.diffState = (reviewBranch, targetBranch) => {
+	return gitRun(['branch'])
+		.then(resultString => {
+			const matcher = /[\s\*]+(.+?)/y;
+			let match = matcher.exec(resultString);
+			let result = [];
+			while (match) {
+				result.push(match[1]);
+				match = matcher.exec(resultString);
+			}
+			let outputbranch = '.vscodeReview';
+			while (result.includes(outputbranch)) {
+				outputbranch += 'x';
+			}
+
+			return gitRun(['checkout', targetBranch])
+				.then(() => gitRun(['checkout', '-b', outputbranch]))
+				.then(() => gitRun(['diff', '--name-status', targetBranch, reviewBranch]))
+				.then(modifiedList => gitRun(['merge', reviewBranch])
+					.then(() => ['a', 'b']));
+		});
+};
 
 exports.branchList = () => {
 	return gitRun(['branch', '-v', '--sort=-committerdate'])
 		.then(resultString => {
-			const matcher = /[\s\*]*(.+?)\s*([a-fA-F0-9]*)\s*(.*)/y;
+			const matcher = /[\s\*]+(.+?)\s+([a-fA-F0-9]+)\s+(.*)/y;
 			let match = matcher.exec(resultString);
 			let result = [];
 			while (match) {
@@ -58,5 +87,5 @@ exports.branchList = () => {
 				match = matcher.exec(resultString);
 			}
 			return result;
-		})
+		});
 };
